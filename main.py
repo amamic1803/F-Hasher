fd50a62f42690ed97ca11487269b31adf21a875b
DarkLord76865 <40371578+darklord76865@users.noreply.github.com>
2022-09-26 23:26:52 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,161 @@
+from blake3 import blake3
+from hashlib import sha1, sha224, sha256, sha384, sha512, sha3_224, sha3_256, sha3_384, sha3_512, blake2s, blake2b, md5
+from zlib import crc32
+
+
+def gen_blake2b(file, hex_hash=True):
+	hasher = blake2b()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_blake2s(file, hex_hash=True):
+	hasher = blake2s()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_blake3(file, hex_hash=True):
+	hasher = blake3(max_threads=blake3.AUTO)
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_crc32(file, hex_hash=True):
+	current_hash = 0
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			current_hash = crc32(chunk, current_hash)
+	if hex_hash:
+		return hex(current_hash)[2:]
+	else:
+		return bin(current_hash)
+
+def gen_md5(file, hex_hash=True):
+	hasher = md5()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_sha1(file, hex_hash=True):
+	hasher = sha1()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_sha224(file, hex_hash=True):
+	hasher = sha224()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_sha256(file, hex_hash=True):
+	hasher = sha256()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_sha384(file, hex_hash=True):
+	hasher = sha384()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_sha512(file, hex_hash=True):
+	hasher = sha512()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_sha3_224(file, hex_hash=True):
+	hasher = sha3_224()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_sha3_256(file, hex_hash=True):
+	hasher = sha3_256()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_sha3_384(file, hex_hash=True):
+	hasher = sha3_384()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+def gen_sha3_512(file, hex_hash=True):
+	hasher = sha3_512()
+	with open(file, "rb") as file:
+		while chunk := file.read(8192):
+			hasher.update(chunk)
+	if hex_hash:
+		return hasher.hexdigest()
+	else:
+		return hasher.digest()
+
+
+if __name__ == "__main__":
+	print(gen_blake2b(r"main.py"))
+	print(gen_blake2s(r"main.py"))
+	print(gen_blake3(r"main.py"))
+	print(gen_crc32(r"main.py"))
+	print(gen_md5(r"main.py"))
+	print(gen_sha1(r"main.py"))
+	print(gen_sha224(r"main.py"))
+	print(gen_sha256(r"main.py"))
+	print(gen_sha384(r"main.py"))
+	print(gen_sha512(r"main.py"))
+	print(gen_sha3_224(r"main.py"))
+	print(gen_sha3_256(r"main.py"))
+	print(gen_sha3_384(r"main.py"))
+	print(gen_sha3_512(r"main.py"))
