1b75203ea18526cd5ca929f632b2a10bf46d46af
DarkLord76865 <mmima191@gmail.com>
2023-12-01 00:06:43 +0100

    update 1.1.1
    refactored code
    added workflows
    improved build.py
    improved README.md

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # F-Hasher
-Get the desired hash value of a selected file.
+Get the hash value of the selected file.
 
 ![screenshot](https://user-images.githubusercontent.com/40371578/192598621-376d9a4f-a04b-4ef7-a711-46408a1bd08d.png)
+
+This is just a nice GUI, the actual hashing is done using dedicated libraries.
 
 ## Downloading
 Prebuilt program is available under Releases
 
 ## Building
 1. Clone repository
-2. Run command: `pip install -r requirements.txt`
-3. Run build.py
-4. Built `*.exe` will be placed in the same folder
+2. Install Python
+3. Install dependencies: `pip install -r requirements.txt`
+4. Install UPX (optional)
+5. Run build.py
+6. Built `*.exe` will be placed in the same folder
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from hashlib import sha1, sha224, sha256, sha384, sha512, sha3_224, sha3_256, sha3_384, sha3_512, blake2s, blake2b, md5
-from os import access, R_OK
 from threading import Thread
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
@@ -42,7 +41,6 @@ def resource_path(relative_path):
 		base_path = os.path.abspath(".")
 	return os.path.join(base_path, relative_path)
 
-
 class CRC32Wrapper:
 	def __init__(self):
 		self.crc = 0
@@ -56,51 +54,13 @@ class CRC32Wrapper:
 	def digest(self):
 		return bin(self.crc)
 
-
-def calculate_hash(file, hash_method, hex_hash=True):
-	hasher = None
-
-	match hash_method:
-		case "BLAKE2B":
-			hasher = blake2b()
-		case "BLAKE2S":
-			hasher = blake2s()
-		case "BLAKE3":
-			hasher = blake3(max_threads=blake3.AUTO)
-		case "CRC32":
-			hasher = CRC32Wrapper()
-		case "MD5":
-			hasher = md5()
-		case "SHA1":
-			hasher = sha1()
-		case "SHA224":
-			hasher = sha224()
-		case "SHA256":
-			hasher = sha256()
-		case "SHA384":
-			hasher = sha384()
-		case "SHA512":
-			hasher = sha512()
-		case "SHA3 224":
-			hasher = sha3_224()
-		case "SHA3 256":
-			hasher = sha3_256()
-		case "SHA3 384":
-			hasher = sha3_384()
-		case "SHA3 512":
-			hasher = sha3_512()
-
-	with open(file, "rb") as file:
-		while chunk := file.read(CHUNK_SIZE):
-			hasher.update(chunk)
-	if hex_hash:
-		return hasher.hexdigest()
-	else:
-		return hasher.digest()
-
-
-class App():
+class App:
 	def __init__(self):
+		self.current_hash_algorithm = HASH_ALGORITHMS[2]
+		self.current_calculated_hash = ""
+		self.state = True  # True = gui enabled, not hashing, False = gui disabled, hashing
+		self.gui_alive = True  # True = gui alive, False = gui destroyed, stops hashing thread
+
 		self.root = tk.Tk()
 		self.root.resizable(False, False)
 		self.root.title("F-Hasher")
@@ -123,14 +83,14 @@ class App():
 		self.hash_lbl.place(x=100, y=110, width=185, height=40)
 
 		self.hash_selector = tk.Label(self.root,
-		                              text=hash_selected, font=("Helvetica", 15, "bold"),
+		                              text=self.current_hash_algorithm, font=("Helvetica", 15, "bold"), cursor="hand2",
 		                              borderwidth=0,
 		                              background="#58c9ff", activebackground="#58c9ff",
 		                              foreground="#ffffff", activeforeground="#ffffff")
 		self.hash_selector.place(x=300, y=110, width=120, height=40)
-		self.hash_selector.bind("<Enter>", lambda event: hover_algo(event, hash_selector, True))
-		self.hash_selector.bind("<Leave>", lambda event: hover_algo(event, hash_selector, False))
-		self.hash_selector.bind("<ButtonRelease-1>", lambda event: hash_select(event))
+		self.hash_selector.bind("<Enter>", lambda event: self.algorithm_btn_hover(event, self.hash_selector, True))
+		self.hash_selector.bind("<Leave>", lambda event: self.algorithm_btn_hover(event, self.hash_selector, False))
+		self.hash_selector.bind("<ButtonRelease-1>", self.select_hash_algorithm)
 
 		self.file_lbl = tk.Label(self.root,
 		                         text="File:", font=("Gabriola", 25, "bold"),
@@ -148,26 +108,26 @@ class App():
 		self.file_ent.place(x=100, y=160, width=425, height=30)
 
 		self.browse_btn = tk.Label(self.root,
-		                           text="Browse", font=("Helvetica", 11, "bold"),
+		                           text="Browse", font=("Helvetica", 11, "bold"), cursor="hand2",
 		                           highlightthickness=1, highlightbackground="#ffffff",
 		                           highlightcolor="#ffffff", borderwidth=0,
 		                           background="#406060", activebackground="#406060",
 		                           foreground="#ffffff", activeforeground="#ffffff")
 		self.browse_btn.place(x=550, y=160, width=75, height=30)
-		self.browse_btn.bind("<Enter>", lambda event: change_thickness(event, browse_btn, False))
-		self.browse_btn.bind("<Leave>", lambda event: change_thickness(event, browse_btn, True))
-		self.browse_btn.bind("<ButtonRelease-1>", browse_click)
+		self.browse_btn.bind("<Enter>", lambda event: self.change_thickness(event, self.browse_btn, True))
+		self.browse_btn.bind("<Leave>", lambda event: self.change_thickness(event, self.browse_btn, False))
+		self.browse_btn.bind("<ButtonRelease-1>", self.browse_files)
 
 		self.start_hashing = tk.Label(self.root,
-		                              text="Hash", font=("Helvetica", 11, "bold"),
+		                              text="Hash", font=("Helvetica", 11, "bold"), cursor="hand2",
 		                              highlightthickness=1, highlightbackground="#ffffff",
 		                              highlightcolor="#ffffff", borderwidth=0,
 		                              background="#406060", activebackground="#406060",
 		                              foreground="#ffffff", activeforeground="#ffffff")
 		self.start_hashing.place(x=650, y=160, width=75, height=30)
-		self.start_hashing.bind("<Enter>", lambda event: change_thickness(event, start_hashing, False))
-		self.start_hashing.bind("<Leave>", lambda event: change_thickness(event, start_hashing, True))
-		self.start_hashing.bind("<ButtonRelease-1>", hash_click)
+		self.start_hashing.bind("<Enter>", lambda event: self.change_thickness(event, self.start_hashing, True))
+		self.start_hashing.bind("<Leave>", lambda event: self.change_thickness(event, self.start_hashing, False))
+		self.start_hashing.bind("<ButtonRelease-1>", self.hash_click)
 
 		self.output_hash = tk.Label(self.root,
 		                            text="", font=("Helvetica", 14, "bold"),
@@ -175,140 +135,166 @@ class App():
 		                            background="#58c9ff", activebackground="#58c9ff",
 		                            foreground="#ffffff", activeforeground="#ffffff")
 		self.output_hash.place(x=0, y=190, width=750, height=60)
-		self.output_hash.bind("<Enter>", lambda event: hover_hash(event, output_hash, True))
-		self.output_hash.bind("<Leave>", lambda event: hover_hash(event, output_hash, False))
-		self.output_hash.bind("<ButtonRelease-1>", lambda event: hash_copy(event))
-
-		global root, file_ent, disabled, hash_selected, hash_selector, start_hashing, output_hash, browse_btn, hashed
-		hashed = ""
-		disabled = False
-
-		hash_selected = HASH_ALGORITHMS[2]
+		self.output_hash.bind("<Enter>", lambda event: self.output_hash_hover(event, self.output_hash, True))
+		self.output_hash.bind("<Leave>", lambda event: self.output_hash_hover(event, self.output_hash, False))
+		self.output_hash.bind("<ButtonRelease-1>", self.copy_hash)
 
 		self.root.mainloop()
 
+	def change_thickness(self, event, widget, enter=True):
+		if self.state:
+			if enter:
+				widget.config(highlightthickness=3)
+			else:
+				widget.config(highlightthickness=1)
 
-def hash_copy(event):
-	global disabled, output_hash, hashed
-	if not disabled and output_hash["text"] != "":
-		pyperclip.copy(hashed)
-		showinfo(title="Copied!", message="Hash value copied to clipboard!", parent=root)
+	def algorithm_btn_hover(self, event, widget, enter=True):
+		if self.state:
+			if enter:
+				widget.config(background="#8AD9FF", activebackground="#8AD9FF")
+			else:
+				widget.config(background="#58c9ff", activebackground="#58c9ff")
 
+	def output_hash_hover(self, event, widget, enter=True):
+		if self.state and self.current_calculated_hash != "":
+			if enter:
+				widget.config(background="#8AD9FF", activebackground="#8AD9FF")
+			else:
+				widget.config(background="#58c9ff", activebackground="#58c9ff")
 
-def hash_select_click(event, widget, toplevel_win):
-	global hash_selected, hash_selector, hashed, output_hash
-	hash_selected = widget["text"]
-	hash_selector.config(text=hash_selected)
-	toplevel_win.destroy()
-	hashed = ""
-	output_hash.config(text="")
+	def select_hash_algorithm(self, event):
+		if self.state:
+			select_hash = SelectHash(self.root)
+			select_hash = select_hash.result()
+			if select_hash != "":
+				self.current_hash_algorithm = select_hash
+				self.hash_selector.config(text=self.current_hash_algorithm)
+				self.current_calculated_hash = ""
+				self.output_hash.config(text="", cursor="arrow")
 
+	def browse_files(self, event):
+		if self.state:
+			initial_dir = os.path.dirname(self.file_ent.get())
+			if not os.path.isdir(initial_dir):
+				initial_dir = os.path.dirname(sys.executable)
+			if not os.path.isdir(initial_dir):
+				initial_dir = os.path.join(os.path.expanduser('~'), 'Desktop')
+			selection = askopenfilename(initialdir=initial_dir, parent=self.root)
+			if os.path.isfile(selection) and os.access(selection, os.R_OK):
+				self.file_ent.delete(0, tk.END)
+				self.file_ent.insert(0, selection.replace("/", "\\"))
+				self.file_ent.xview_moveto(1)
 
-def hash_select(event):
-	global root, disabled
-	if not disabled:
-		hash_btns = []
+	def copy_hash(self, event):
+		if self.state and self.current_calculated_hash != "":
+			pyperclip.copy(self.current_calculated_hash)
+			showinfo(title="Copied!", message="Hash value copied to clipboard!", parent=self.root)
 
-		select_window = tk.Toplevel(root, background="light blue")
-		select_window.title("Select hash!")
-		select_window.geometry(f"250x560+{root.winfo_screenwidth() // 2 - 125}+{root.winfo_screenheight() // 2 - 280}")
-		select_window.resizable(False, False)
-		select_window.iconbitmap(resource_path("resources\\hash-icon.ico"))
-		select_window.grab_set()
-		select_window.focus()
+	def hash_click(self, event):
+		file_path = self.file_ent.get()
+
+		if os.path.isfile(file_path) and os.access(file_path, os.R_OK):
+			hash_thread = Thread(target=self.calculate_hash, args=(file_path, self.current_hash_algorithm))
+			hash_thread.start()
+			self.state = False
+			self.start_hashing.config(highlightthickness=1, text="Hashing", background="#263939", activebackground="#263939", cursor="arrow")
+			self.browse_btn.config(background="#263939", activebackground="#263939", cursor="arrow")
+			self.file_ent.config(state="disabled")
+			self.output_hash.config(text="", cursor="arrow")
+			self.hash_selector.config(cursor="arrow")
+		else:
+			showerror(title="Invalid file!", message="The selected file can't be processed!", parent=self.root)
+
+	def calculate_hash(self, file, hash_method, hex_hash=True):
+		hasher = None
+
+		match hash_method:
+			case "BLAKE2B":
+				hasher = blake2b()
+			case "BLAKE2S":
+				hasher = blake2s()
+			case "BLAKE3":
+				hasher = blake3(max_threads=blake3.AUTO)
+			case "CRC32":
+				hasher = CRC32Wrapper()
+			case "MD5":
+				hasher = md5()
+			case "SHA1":
+				hasher = sha1()
+			case "SHA224":
+				hasher = sha224()
+			case "SHA256":
+				hasher = sha256()
+			case "SHA384":
+				hasher = sha384()
+			case "SHA512":
+				hasher = sha512()
+			case "SHA3 224":
+				hasher = sha3_224()
+			case "SHA3 256":
+				hasher = sha3_256()
+			case "SHA3 384":
+				hasher = sha3_384()
+			case "SHA3 512":
+				hasher = sha3_512()
+
+		with open(file, "rb") as file:
+			while (chunk := file.read(CHUNK_SIZE)) and self.gui_alive:
+				hasher.update(chunk)
+
+		self.update_hash(hasher.hexdigest() if hex_hash else hasher.digest())
+
+	def update_hash(self, hash_value):
+		self.state = True
+		self.start_hashing.config(text="Hash", background="#406060", activebackground="#406060", cursor="hand2")
+		self.browse_btn.config(background="#406060", activebackground="#406060", cursor="hand2")
+		self.file_ent.config(state="normal")
+		if hash_value != "":
+			self.output_hash.config(cursor="hand2")
+		self.hash_selector.config(cursor="hand2")
+		self.current_calculated_hash = hash_value
+
+		if len(hash_value) > 64:
+			self.output_hash.config(text=f"{hash_value[:32]}...{hash_value[-32:]}")
+		else:
+			self.output_hash.config(text=hash_value)
+
+class SelectHash:
+	def __init__(self, root):
+		self.result_str = ""
+		self.window = tk.Toplevel(root, background="light blue")
+		self.window.title("Select hash!")
+		self.window.geometry(f"250x560+{root.winfo_screenwidth() // 2 - 125}+{root.winfo_screenheight() // 2 - 280}")
+		self.window.resizable(False, False)
+		self.window.iconbitmap(resource_path("resources\\hash-icon.ico"))
+		self.window.grab_set()
+		self.window.focus()
+
+		self.hash_btns = []
 
 		curr_y = 0
 		for i in HASH_ALGORITHMS:
-			hash_btns.append(tk.Label(select_window,
-			                          text=i, font=("Helvetica", 15, "bold"),
-			                          borderwidth=0, highlightthickness=0,
-			                          background="light blue", activebackground="light blue",
-			                          foreground="#ffffff", activeforeground="#ffffff"))
-			hash_btns[-1].place(x=0, y=curr_y, width=250, height=40)
-			hash_btns[-1].bind("<Enter>", lambda event, widget=hash_btns[-1]: widget.config(background="#B5E2F0",
-			                                                                                activebackground="#B5E2F0"))
-			hash_btns[-1].bind("<Leave>", lambda event, widget=hash_btns[-1]: widget.config(background="light blue",
-			                                                                                activebackground="light blue"))
-			hash_btns[-1].bind("<ButtonRelease-1>",
-			                   lambda event, widget=hash_btns[-1]: hash_select_click(event, widget, select_window))
+			self.hash_btns.append(tk.Label(self.window,
+			                               text=i, font=("Helvetica", 15, "bold"), cursor="hand2",
+			                               borderwidth=0, highlightthickness=0,
+			                               background="light blue", activebackground="light blue",
+			                               foreground="#ffffff", activeforeground="#ffffff"))
+			self.hash_btns[-1].place(x=0, y=curr_y, width=250, height=40)
+			self.hash_btns[-1].bind("<Enter>",
+			                        lambda event, widget=self.hash_btns[-1]: widget.config(background="#B5E2F0", activebackground="#B5E2F0"))
+			self.hash_btns[-1].bind("<Leave>",
+			                        lambda event, widget=self.hash_btns[-1]: widget.config(background="light blue", activebackground="light blue"))
+			self.hash_btns[-1].bind("<ButtonRelease-1>",
+			                        lambda event, widget=self.hash_btns[-1]: self.click(event, widget))
 			curr_y += 40
+		self.window.wait_window()
 
-		select_window.wait_window()
+	def click(self, event, widget):
+		self.result_str = widget["text"]
+		self.window.destroy()
 
-
-def hover_algo(event, widget, typ):
-	global disabled
-	if not disabled:
-		if typ:
-			widget.config(background="#8AD9FF", activebackground="#8AD9FF")
-		else:
-			widget.config(background="#58c9ff", activebackground="#58c9ff")
-
-
-def hover_hash(event, widget, typ):
-	global disabled
-	if not disabled and widget["text"] != "":
-		if typ:
-			widget.config(background="#8AD9FF", activebackground="#8AD9FF")
-		else:
-			widget.config(background="#58c9ff", activebackground="#58c9ff")
-
-
-def change_thickness(event, widget, typ):
-	global disabled
-	if not disabled:
-		if typ:
-			widget.config(highlightthickness=1)
-		else:
-			widget.config(highlightthickness=3)
-
-
-def browse_click(event):
-	global root, file_ent, disabled
-	if not disabled:
-		init_dir = os.path.dirname(file_ent.get())
-		if not os.path.isdir(init_dir):
-			init_dir = os.path.dirname(sys.executable)
-		if not os.path.isdir(init_dir):
-			init_dir = os.path.join(os.path.expanduser('~'), 'Desktop')
-		selection = askopenfilename(initialdir=init_dir, parent=root)
-		if os.path.isfile(selection) and access(selection, R_OK):
-			file_ent.delete(0, tk.END)
-			file_ent.insert(0, selection.replace("/", "\\"))
-			file_ent.xview_moveto(1)
-
-
-def hash_file(file, hash_method):
-	global disabled, file_ent, start_hashing, output_hash, browse_btn, hashed
-
-	hashed = calculate_hash(file, hash_method)
-
-	disabled = False
-	start_hashing.config(text="Hash", background="#406060", activebackground="#406060")
-	browse_btn.config(background="#406060", activebackground="#406060")
-	file_ent.config(state="normal")
-
-	if len(hashed) > 64:
-		output_hash.config(text=f"{hashed[:32]}...{hashed[-32:]}")
-	else:
-		output_hash.config(text=hashed)
-
-
-def hash_click(event):
-	global disabled, file_ent, root, start_hashing, hash_selected, browse_btn
-
-	content = file_ent.get()
-
-	if os.path.isfile(content) and access(content, R_OK):
-		hash_thread = Thread(target=hash_file, args=(content, hash_selected))
-		hash_thread.start()
-		disabled = True
-		start_hashing.config(highlightthickness=1, text="Hashing", background="#263939", activebackground="#263939")
-		browse_btn.config(background="#263939", activebackground="#263939")
-		file_ent.config(state="disabled")
-	else:
-		showerror(title="Invalid file!", message="The selected file can't be processed!", parent=root)
-
+	def result(self) -> str:
+		return self.result_str
 
 def main():
 	App()
